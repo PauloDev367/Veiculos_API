d566127a934d539facdb43d75b74f1f4d1407f0f
Paulo Silva <pauloecomercial367@gmail.com>
2025-02-16 16:30:56 -0300

    :sparkles: salvando fotos de um veiculo

--- VeiculosApi/Controllers/PhotoController.cs
+++ VeiculosApi/Controllers/PhotoController.cs
@@ -1,4 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
+using VeiculosApi.Http.Request;
+using VeiculosApi.Http.Response;
+using VeiculosApi.Models;
+using VeiculosApi.Services;
 
 namespace VeiculosApi.Controllers;
 
@@ -6,4 +10,26 @@ namespace VeiculosApi.Controllers;
 [ApiController]
 public class PhotoController : ControllerBase
 {
+    private readonly PhotoService _photoService;
+    private readonly SavePhotoService _savePhotoService;
+
+    public PhotoController(PhotoService photoService, SavePhotoService savePhotoService)
+    {
+        _photoService = photoService;
+        _savePhotoService = savePhotoService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddAsync([FromForm] AddPhotosToVehicleRequest request)
+    {
+        var paths = await _savePhotoService.SaveVehiclePhotosAsync(request.Photos);
+        var created = await _photoService.AddPhotoAsync(paths, request.VehicleId);
+
+        return Ok(new DefaultControllerResponse<List<Photo>>
+        {
+            Status = 200,
+            Message = "Photos added successfully",
+            Data = created
+        });
+    }
 }
--- VeiculosApi/Http/Request/AddPhotosToVehicleRequest.cs
+++ VeiculosApi/Http/Request/AddPhotosToVehicleRequest.cs
@@ -1,9 +1,12 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace VeiculosApi.Http.Request;
 
 public class AddPhotosToVehicleRequest
 {
+    [Required]
     public List<IFormFile> Photos { get; set; }
+    [Required]
     public Guid VehicleId { get; set; }
 }
